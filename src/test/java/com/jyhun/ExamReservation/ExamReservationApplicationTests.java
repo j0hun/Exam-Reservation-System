419b2dd40dffacfa43ff213a566b074b1203129a
j0hun <dudgns3681@gmail.com>
2024-11-20 17:31:43 +0900

    FEAT: 프로젝트 초기 설정 - Spring Boot 프로젝트를 새로 생성하고 기본 설정을 완료했습니다. - 필요한 스타터 패키지와 의존성을 추가했습니다.

---
+++ src/test/java/com/jyhun/ExamReservation/ExamReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyhun.ExamReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
